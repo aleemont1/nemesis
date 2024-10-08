b39c8240fe19e02c2fb61f719e0d43d625619b8f
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-10-08 12:55:58 +0200

    [BME680] Refactor BME680Sensor class

--- src/sensors/BME680/BME680Sensor.cpp
+++ src/sensors/BME680/BME680Sensor.cpp
@@ -1,34 +1,60 @@
 #include "BME680Sensor.h"
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
 #include <Adafruit_BME680.h>
+#include "sensors/BME680/BME680Sensor.h"
+
+bool initialized = false;
 
 BME680Sensor::BME680Sensor()
 {
-}
-
-BME680Sensor::~BME680Sensor()
-{
-    delete &data;
+    bme = Adafruit_BME680(&Wire);
 }
 
 bool BME680Sensor::init()
 {
-    return false;
+    int attempts = 0;
+    while (!bme.begin() && attempts++ < SENSOR_LOOKUP_MAX_ATTEMPTS)
+    {
+        Serial.println("Could not find a valid BME680 sensor, check wiring!");
+        delay(1000);
+    }
+    if(attempts >= SENSOR_LOOKUP_MAX_ATTEMPTS)
+    {
+        return initialized;
+    }
+    // Set up oversampling and filter initialization (NOTE: need to study the optimal values)
+    bme.setTemperatureOversampling(BME680_OS_8X);   // Set temperature oversampling
+    bme.setHumidityOversampling(BME680_OS_2X);      // Set humidity oversampling
+    bme.setPressureOversampling(BME680_OS_4X);      // Set pressure oversampling
+    bme.setIIRFilterSize(BME680_FILTER_SIZE_3);     // Set filter size
+    bme.setGasHeater(0, 0);                         // Disable gas heater
+    initialized = true;
+    return initialized;
 }
 
 bool BME680Sensor::readData()
 {
-    return false;
-}
-
-bool BME680Sensor::readData_async()
-{
-    return false;
+    if (!initialized)
+    {
+        Serial.println("BME680 Sensor not initialized");
+        return false;
+    }
+    if (!bme.performReading())
+    {
+        Serial.println("BME680 Failed to perform reading :(");
+        return false;
+    } 
+    Serial.println("BME680 Reading performed successfully");
+    return true;
 }
 
 BME680SensorData BME680Sensor::getData()
 {
-    return BME680SensorData(data.getTemperature(), 
-                            data.getPressure(), 
-                            data.getHumidity(), 
-                            data.getGasResistance());
+    if (!initialized)
+    {
+        Serial.println("BME680 Sensor not initialized");
+        return BME680SensorData();
+    }
+    return BME680SensorData(bme.temperature, bme.pressure, bme.humidity, bme.gas_resistance);
 }
--- src/sensors/BME680/BME680Sensor.h
+++ src/sensors/BME680/BME680Sensor.h
@@ -1,3 +1,5 @@
+#ifndef BME680_SENSOR_H
+#define BME680_SENSOR_H
 /**
  * @class BME680Sensor
  * @brief A sensor class for the BME680 sensor, inheriting from the Sensor class template.
@@ -16,37 +18,18 @@
  * @author luca.pulga@studio.unibo.it
  */
 #include "sensors/Sensor.h"
-#include "sensors/BME680/BME680SensorData.h"
 #include "const/pins.h"
+#include "const/config.h"
+#include "BME680SensorData.h"
 
 class BME680Sensor : public Sensor<BME680SensorData>
 {
 public:
     BME680Sensor();
-    ~BME680Sensor();
-
     bool init() override;
     bool readData() override;
     BME680SensorData getData() override;
-
-    /**
-     * @brief Start an asynchronous reading of the sensor data.
-     *
-     * @return true if the reading started successfully
-     * @return false if the reading failed to start
-     */
-    bool readData_async_start();
-
-    /**
-     * @brief End an asynchronous reading of the sensor data.
-     *        If no reading is in progress, the method will
-     *        perform a blocking read.
-     * @return true if the reading ended successfully
-     * @return false if the reading failed to end
-     */
-    bool readData_async_end();
-
 private:
-    BME680SensorData data;  // sensor data object
-    Adafruit_BME680 bme680; // BME680 sensor object
+    Adafruit_BME680 bme;
 };
+#endif // BME680_SENSOR_H
