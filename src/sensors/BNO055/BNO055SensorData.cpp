66f55008dd74d4e21476f630c7427ad650604e40
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-10-08 17:16:56 +0200

    [BNO055] Improve BNO055SensorData interface and implementation

--- src/sensors/BNO055/BNO055SensorData.cpp
+++ src/sensors/BNO055/BNO055SensorData.cpp
@@ -1,31 +1,31 @@
 #include "BNO055SensorData.h"
 
-sensors_event_t BNO055SensorData::getOrientation() const
+sensors_vec_t BNO055SensorData::getOrientation() const
 {
     return this->orientation;
 }
 
-sensors_event_t BNO055SensorData::getAngularVelocity() const
+sensors_vec_t BNO055SensorData::getAngularVelocity() const
 {
     return this->angularVelocity;
 }
 
-sensors_event_t BNO055SensorData::getLinearAcceleration() const
+sensors_vec_t BNO055SensorData::getLinearAcceleration() const
 {
     return this->linearAcceleration;
 }
 
-sensors_event_t BNO055SensorData::getMagnetometer() const
+sensors_vec_t BNO055SensorData::getMagnetometer() const
 {
     return this->magnetometer;
 }
 
-sensors_event_t BNO055SensorData::getAccelerometer() const
+sensors_vec_t BNO055SensorData::getAccelerometer() const
 {
     return this->accelerometer;
 }
 
-sensors_event_t BNO055SensorData::getGravity() const
+sensors_vec_t BNO055SensorData::getGravity() const
 {
     return this->gravity;
 }
@@ -57,13 +57,20 @@ uint8_t BNO055SensorData::getMagCalibration() const
 
 String BNO055SensorData::toString()
 {
-    String result = "Orientation: [" + String(this->orientation.orientation.x) + ", " + String(this->orientation.orientation.y) + ", " + String(this->orientation.orientation.z) + "]\n";
-    result += "Angular Velocity: [" + String(this->angularVelocity.gyro.x) + ", " + String(this->angularVelocity.gyro.y) + ", " + String(this->angularVelocity.gyro.z) + "]\n";
-    result += "Linear Acceleration: [" + String(this->linearAcceleration.acceleration.x) + ", " + String(this->linearAcceleration.acceleration.y) + ", " + String(this->linearAcceleration.acceleration.z) + "]\n";
-    result += "Magnetometer: [" + String(this->magnetometer.magnetic.x) + ", " + String(this->magnetometer.magnetic.y) + ", " + String(this->magnetometer.magnetic.z) + "]\n";
-    result += "Accelerometer: [" + String(this->accelerometer.acceleration.x) + ", " + String(this->accelerometer.acceleration.y) + ", " + String(this->accelerometer.acceleration.z) + "]\n";
-    result += "Gravity: [" + String(this->gravity.acceleration.x) + ", " + String(this->gravity.acceleration.y) + ", " + String(this->gravity.acceleration.z) + "]\n";
-    result += "Board Temperature: " + String(this->board_temperature) + " °C\n";
-    result += "Calibration Levels: Sys=" + String(this->system_calibration) + " Gyro=" + String(this->gyro_calibration) + " Accel=" + String(this->accel_calibration) + " Mag=" + String(this->mag_calibration);
+    sensors_vec_t orient = this->getOrientation();
+    sensors_vec_t angVel = this->getAngularVelocity();
+    sensors_vec_t linAcc = this->getLinearAcceleration();
+    sensors_vec_t mag = this->getMagnetometer();
+    sensors_vec_t accel = this->getAccelerometer();
+    sensors_vec_t grav = this->getGravity();
+
+    String result = "Orientation: [" + String(orient.x) + ", " + String(orient.y) + ", " + String(orient.z) + "]\n";
+    result += "Angular Velocity: [" + String(angVel.x) + ", " + String(angVel.y) + ", " + String(angVel.z) + "]\n";
+    result += "Linear Acceleration: [" + String(linAcc.x) + ", " + String(linAcc.y) + ", " + String(linAcc.z) + "]\n";
+    result += "Magnetometer: [" + String(mag.x) + ", " + String(mag.y) + ", " + String(mag.z) + "]\n";
+    result += "Accelerometer: [" + String(accel.x) + ", " + String(accel.y) + ", " + String(accel.z) + "]\n";
+    result += "Gravity: [" + String(grav.x) + ", " + String(grav.y) + ", " + String(grav.z) + "]\n";
+    result += "Board Temperature: " + String(board_temperature) + " °C\n";
+    result += "Calibration Levels: Sys=" + String(system_calibration) + " Gyro=" + String(gyro_calibration) + " Accel=" + String(accel_calibration) + " Mag=" + String(mag_calibration);
     return result;
 }
