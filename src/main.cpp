746ffb7359d78fba386bd5338639472bc6cf4c32
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-10-10 16:42:00 +0200

    [LOGGER] Test sensor data logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Wire.h>
 #include "sensors/BME680/BME680Sensor.h"
 #include "sensors/BNO055/BNO055Sensor.h"
+#include "utils/logger/CborLogger.h"
 
 BME680Sensor bme680;
 BME680Sensor bme680_2;
 BNO055Sensor bno055;
+CborLogger cbor_logger;
 
 void setup()
 {
     Serial.begin(115200);
-    while (!Serial);
+    while (!Serial)
+        ;
     Serial.println("BME680 & BNO055 test");
     bme680.init(BME680_I2C_ADDR_1);
     bme680_2.init(BME680_I2C_ADDR_2);
@@ -19,13 +22,18 @@ void setup()
 void loop()
 {
     bme680.readData();
-    Serial.println("################# BME680 ################# ");
-    Serial.println(bme680.getData().toString());
+    // Serial.println("################# BME680 ################# ");
+    // Serial.println(bme680.getData().toString());
+    bme680.getData().log(cbor_logger);
     bme680_2.readData();
-    Serial.println("################# BME680_2 #################");
-    Serial.println(bme680_2.getData().toString());
+    // Serial.println("################# BME680_2 #################");
+    // Serial.println(bme680_2.getData().toString());
+    bme680_2.getData().log(cbor_logger);
     bno055.readData();
-    Serial.println("################# BNO055 #################");
-    Serial.println(bno055.getData().toString());
+    // Serial.println("################# BNO055 #################");
+    // Serial.println(bno055.getData().toString());
+    bno055.getData().log(cbor_logger);
+    cbor_logger.log();
+    cbor_logger.clear();
     delay(1000);
 }
