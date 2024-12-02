0e0b46f42fe7c8f419941ac2d832a178a5375ddd
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2024-12-02 18:21:38 +0100

    [REFACTORING] Move config and pins definitions to global dir

--- src/global/config.h
+++ src/global/config.h
@@ -1,13 +1,19 @@
+#include <LoRa_E220.h>
+
 #define SENSOR_LOOKUP_MAX_ATTEMPTS 5
 #define SENSOR_LOOKUP_TIMEOUT 1000
 #define BME680_I2C_ADDR_1 0x77
 #define BME680_I2C_ADDR_2 0x76
 #define BNO055_I2C_ADDR 0x28
-#define SEALEVELPRESSURE_HPA (1013.25)
 
-// LoRa configuration
+/* LoRa configuration */
+#define LORA_ADDH 0x00
+#define LORA_ADDL 0x03
+
 #define LORA_DESTINATION_ADDH 0x00
 #define LORA_DESTINATION_ADDL 0x05
 #define LORA_CHANNEL 23
+/* Uncomment the following line to enable RSSI */
+#define ENABLE_RSSI
 
-
+#define SERIAL_BAUD_RATE 115200
