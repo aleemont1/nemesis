2d71837aab025e3edc43896cd5fb323c94a066e9
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-11 16:55:35 +0100

    [SD] Rewrite SD interface

--- src/utils/logger/SD/SD-master.hpp
+++ src/utils/logger/SD/SD-master.hpp
@@ -1,18 +1,18 @@
+#include <variant>
+#include <Arduino.h>
+#include "SdFat.h"
+#include "const/pins.h"
+class SD
+{
+private:
+    SdFat SD;
+    SdFile *file;
 
-
-class _SD {
-    private:
-        
-        bool success=false;
-        
-
-    public:
-        File reporter;
-        bool init(int CE_);
-        bool write_file(std::string _name_file, std::string _string); //se true file trovato e scritto, se false file non trovato
-        bool read_file(std::string _name_file); //stampa il contenuto del file. ritorna true se tutto ok senno no
-        bool remove(std::string dir_file); //se false, la directory o il file non sono stati eliminati correttamente
-        bool clear_sd(); //cancella tutto il contenuto dell'sd
-
-
-}
+public:
+    bool init();
+    bool openFile(std::string filename);
+    bool closeFile();
+    bool writeFile(std::string filename, std::variant<std::string, String, char *> content); // se true file trovato e scritto, se false file non trovato
+    char *readFile();                                                             // stampa il contenuto del file. ritorna true se tutto ok senno no
+    bool clearSD();                                                                          // cancella tutto il contenuto dell'sd
+};
