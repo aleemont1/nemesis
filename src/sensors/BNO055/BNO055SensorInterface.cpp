9f61fbf5adadf54d51dd8782342b5e73a3ce7ef9
Giacomo Antonelli <giacomo.antonelli3@gmail.com>
2025-07-23 21:28:20 +0200

    Implementation of the core driver functions for BNO055 + Comments

---
+++ src/sensors/BNO055/BNO055SensorInterface.cpp
@@ -0,0 +1,287 @@
+#include "BNO055SensorInterface.h"
+#include "const/config.h"
+
+BNO055SensorInterface::BNO055SensorInterface() {}
+
+bool BNO055SensorInterface::init()
+{
+    return bno055_init(&bno) == BNO055_SUCCESS;
+}
+
+bool BNO055SensorInterface::calibrate()
+{    
+    // Check calibration status for all components
+    return (calibrate_accel() && calibrate_mag() && 
+            calibrate_gyro() && calibrate_sys());
+}
+
+bool BNO055SensorInterface::calibrate_accel() {
+    uint8_t accel_calib = 0;
+    
+    // Get current accelerometer calibration status from BNO055 registers
+    bool success = (bno055_get_accel_calib_stat(&accel_calib) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and accelerometer has some calibration
+    return (success && accel_calib > 0);
+}
+
+bool BNO055SensorInterface::calibrate_mag() {
+    uint8_t mag_calib = 0;
+    
+    // Get current magnetometer calibration status from BNO055 registers
+    bool success = (bno055_get_mag_calib_stat(&mag_calib) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and magnetometer has some calibration
+    return (success && mag_calib > 0);
+}
+
+bool BNO055SensorInterface::calibrate_gyro() {
+    uint8_t gyro_calib = 0;
+    
+    // Get current gyroscope calibration status from BNO055 registers
+    bool success = (bno055_get_gyro_calib_stat(&gyro_calib) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and gyroscope has some calibration
+    return (success && gyro_calib > 0);
+}
+
+bool BNO055SensorInterface::calibrate_sys() {
+    uint8_t sys_calib = 0;
+    
+    // Get current system calibration status from BNO055 registers
+    bool success = (bno055_get_sys_calib_stat(&sys_calib) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and system has some calibration
+    return (success && sys_calib > 0);
+}
+
+bool BNO055SensorInterface::selftest_accel() {
+    uint8_t selftest_result = 0;
+    
+    // Get accelerometer self-test result from register 0x36 bit 0
+    bool success = (bno055_get_selftest_accel(&selftest_result) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and test passed (0x01 = pass, 0x00 = fail)
+    return (success && selftest_result == 0x01);
+}
+
+bool BNO055SensorInterface::selftest_mag() {
+    uint8_t selftest_result = 0;
+    
+    // Get magnetometer self-test result from register 0x36 bit 1
+    bool success = (bno055_get_selftest_mag(&selftest_result) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and test passed (0x01 = pass, 0x00 = fail)
+    return (success && selftest_result == 0x01);
+}
+
+bool BNO055SensorInterface::selftest_gyro() {
+    uint8_t selftest_result = 0;
+    
+    // Get gyroscope self-test result from register 0x36 bit 2
+    bool success = (bno055_get_selftest_gyro(&selftest_result) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and test passed (0x01 = pass, 0x00 = fail)
+    return (success && selftest_result == 0x01);
+}
+
+bool BNO055SensorInterface::selftest_mcu() {
+    uint8_t selftest_result = 0;
+    
+    // Get microcontroller self-test result from register 0x36 bit 3
+    bool success = (bno055_get_selftest_mcu(&selftest_result) == BNO055_SUCCESS);
+    
+    // Return true if read was successful and test passed (0x01 = pass, 0x00 = fail)
+    return (success && selftest_result == 0x01);
+}
+
+bool BNO055SensorInterface::check_system_status() {
+    uint8_t sys_stat = 0;
+    
+    // Get system status from register 0x39
+    bool success = (bno055_get_sys_stat_code(&sys_stat) == BNO055_SUCCESS);
+    
+    // Return true if system is running properly (status 5 or 6)
+    return (success && (sys_stat == 5 || sys_stat == 6));
+}
+
+bool BNO055SensorInterface::check_system_error() {
+    uint8_t sys_error = 0;
+    
+    // Get system error code from register 0x3A
+    bool success = (bno055_get_sys_error_code(&sys_error) == BNO055_SUCCESS);
+    
+    // Return true if no error (error code 0)
+    return (success && sys_error == 0);
+}
+
+bool BNO055SensorInterface::check_clock_status() {
+    uint8_t clk_stat = 0;
+    
+    // Get main clock status from register 0x38
+    bool success = (bno055_get_stat_main_clk(&clk_stat) == BNO055_SUCCESS);
+    
+    // Return true if clock is running properly (status 1)
+    return (success && clk_stat == 1);
+}
+
+uint8_t BNO055SensorInterface::get_system_error_code() {
+    uint8_t sys_error = 0;
+    bno055_get_sys_error_code(&sys_error);
+    return sys_error;
+}
+
+uint8_t BNO055SensorInterface::get_system_status_code() {
+    uint8_t sys_stat = 0;
+    bno055_get_sys_stat_code(&sys_stat);
+    return sys_stat;
+}
+
+bool BNO055SensorInterface::set_operation_mode(uint8_t mode) {
+    return (bno055_set_operation_mode(mode) == BNO055_SUCCESS);
+}
+
+bool BNO055SensorInterface::get_operation_mode(uint8_t* mode) {
+    return (bno055_get_operation_mode(mode) == BNO055_SUCCESS);
+}
+
+bool BNO055SensorInterface::set_power_mode(uint8_t mode) {
+    return (bno055_set_power_mode(mode) == BNO055_SUCCESS);
+}
+
+bool BNO055SensorInterface::get_power_mode(uint8_t* mode) {
+    return (bno055_get_power_mode(mode) == BNO055_SUCCESS);
+}
+
+bool BNO055SensorInterface::set_accel_power_mode(uint8_t mode) {
+    return (bno055_set_accel_power_mode(mode) == BNO055_SUCCESS);
+}
+
+bool BNO055SensorInterface::set_mag_power_mode(uint8_t mode) {
+    return (bno055_set_mag_power_mode(mode) == BNO055_SUCCESS);
+}
+
+bool BNO055SensorInterface::set_gyro_power_mode(uint8_t mode) {
+    return (bno055_set_gyro_power_mode(mode) == BNO055_SUCCESS);
+}
+
+struct bno055_accel_float_t BNO055SensorInterface::get_accel() {
+    struct bno055_accel_t accel;
+    
+    // The function return a boolean value indicating success or failure
+    bool res = bno055_read_accel_xyz(&accel);
+    
+    // Conversion from raw to meterpersecseq
+    struct bno055_accel_float_t accel_msq;
+    bool res2 = bno055_convert_float_accel_xyz_msq(&accel_msq);
+    
+    return accel_msq;
+}
+
+struct bno055_mag_float_t BNO055SensorInterface::get_mag() {
+    struct bno055_mag_t mag;
+
+    // The function return a boolean value indicating success or failure
+    bool res = bno055_read_mag_xyz(&mag);
+
+    // Conversion from raw to microtesla
+    struct bno055_mag_float_t mag_mt;
+    bool res2 = bno055_convert_float_mag_xyz_uT(&mag_mt);
+
+    return mag_mt;
+}
+
+struct bno055_gyro_float_t BNO055SensorInterface::get_gyro_dps() {
+    struct bno055_gyro_t gyro;
+
+    // The function return a boolean value indicating success or failure
+    bool res = bno055_read_gyro_xyz(&gyro);
+
+    // Conversion from raw to degrees per second (dps)
+    struct bno055_gyro_float_t gyro_dps;
+    bool res2 = bno055_convert_float_gyro_xyz_dps(&gyro_dps);
+
+    return gyro_dps;
+}
+
+struct bno055_gyro_float_t  BNO055SensorInterface::get_gyro_rps() {
+    struct bno055_gyro_t gyro;
+
+    // The function return a boolean value indicating success or failure
+    bool res = bno055_read_gyro_xyz(&gyro);
+
+    // Conversion from raw to radians per second (rps)
+    struct bno055_gyro_float_t gyro_rps;
+    bool res2 = bno055_convert_float_gyro_xyz_rps(&gyro_rps);
+
+    return gyro_rps;
+}
+
+struct bno055_euler_float_t BNO055SensorInterface::get_euler_deg() {
+    struct bno055_euler_t euler;
+
+    // The function return a boolean value indicating success or failure
+    bool res = bno055_read_euler_hrp(&euler);
+
+    // Conversion from raw to degrees
+    struct bno055_euler_float_t euler_deg;
+    bool res2 = bno055_convert_float_euler_hpr_deg(&euler_deg);
+
+    return euler_deg;
+}
+
+struct bno055_euler_float_t BNO055SensorInterface::get_euler_rad() {
+    struct bno055_euler_t euler;
+    
+    // The function return a boolean value indicating success or failure
+    bool res = bno055_read_euler_hrp(&euler);
+
+    // Conversion from raw to radians
+    struct bno055_euler_float_t euler_rad;
+    bool res2 = bno055_convert_float_euler_hpr_rad(&euler_rad);
+
+    return euler_rad;
+}
+
+struct bno055_quaternion_t BNO055SensorInterface::get_quaternion() {
+    struct bno055_quaternion_t quaternion;
+    
+    bool res = bno055_read_quaternion_wxyz(&quaternion);
+
+    return quaternion;
+}
+
+struct bno055_linear_accel_t BNO055SensorInterface::get_linear_accel() {
+    struct bno055_linear_accel_t linear_accel;
+    
+    bool res = bno055_read_linear_accel_xyz(&linear_accel);
+
+    // Conversion from raw to meter per second squared (m/s^2)
+    struct bno055_linear_accel_float_t linear_accel_msq;
+    bool res2 = bno055_convert_float_linear_accel_xyz_msq(&linear_accel_msq);
+
+    return linear_accel;
+}
+
+struct bno055_gravity_t BNO055SensorInterface::get_gravity() {
+    struct bno055_gravity_t gravity;
+    
+    bool res = bno055_read_gravity_xyz(&gravity);
+
+    // Conversion from raw to meter per second squared (m/s^2)
+    struct bno055_gravity_float_t gravity_msq;
+    bool res2 = bno055_convert_float_gravity_xyz_msq(&gravity_msq);
+
+    return gravity;
+}
+
+// !!! CHECK THE CONVERTION IN PRACTICE
+float BNO055SensorInterface::get_temperature() {
+    int8_t raw_temp = 0;
+    
+    bool res = (bno055_read_temp_data(&raw_temp) == BNO055_SUCCESS);
+    
+    // Conversion: 1 LSB = 1°C, so just cast to float
+    return static_cast<float>(raw_temp);
+}
+
---
+++ src/sensors/BNO055/BNO055SensorInterface.h
@@ -0,0 +1,269 @@
+#ifndef BNO055_SENSOR_INTERFACE_HPP
+#define BNO055_SENSOR_INTERFACE_HPP
+
+#include "../lib/BNO055_SensorAPI/bno055.h"
+#include "sensors/ISensor.hpp"
+
+// !!! Errors given by the BNO APIs should be managed, maybe with a Publisher-Subscriber pattern?
+
+/**
+ * @brief Interface for BNO055 sensor operations, using low-level APIs from the BNO055_SensorAPI official library, instead of Adafruit_BNO055.
+ */
+class BNO055SensorInterface
+{
+private:
+    bno055_t bno;
+    
+public:
+    /**
+     * @brief Default constructor for BNO055SensorInterface, the actual sensor structure will be initialized in init()
+     */
+    BNO055SensorInterface();
+    
+    /**
+     * @brief Initialize the BNO055 sensor
+     * @return true if initialization successful, false otherwise
+     */
+    bool init();
+    
+    /**
+     * @brief Check if all sensor components are calibrated
+     * @return true if all sensors (accel, mag, gyro, system) are calibrated, false otherwise
+     */
+    bool calibrate();
+    
+    // ========== INDIVIDUAL CALIBRATION FUNCTIONS ==========
+    /**
+     * @brief Check accelerometer calibration status
+     * @return true if accelerometer is calibrated (calibration level > 0), false otherwise
+     */
+    bool calibrate_accel();
+    
+    /**
+     * @brief Check magnetometer calibration status
+     * @return true if magnetometer is calibrated (calibration level > 0), false otherwise
+     * @note Magnetometer calibration typically requires figure-8 movements
+     */
+    bool calibrate_mag();
+    
+    /**
+     * @brief Check gyroscope calibration status
+     * @return true if gyroscope is calibrated (calibration level > 0), false otherwise
+     * @note Gyroscope calibration requires the sensor to remain stationary
+     */
+    bool calibrate_gyro();
+    
+    /**
+     * @brief Check system-wide calibration status
+     * @return true if system calibration is complete (calibration level > 0), false otherwise
+     */
+    bool calibrate_sys();
+    
+    // ========== SELF-TEST FUNCTIONS ==========
+    /**
+     * @brief Perform accelerometer hardware self-test
+     * @return true if accelerometer hardware test passes (result = 0x01), false if fails (result = 0x00)
+     */
+    bool selftest_accel();
+    
+    /**
+     * @brief Perform magnetometer hardware self-test
+     * @return true if magnetometer hardware test passes (result = 0x01), false if fails (result = 0x00)
+     */
+    bool selftest_mag();
+    
+    /**
+     * @brief Perform gyroscope hardware self-test
+     * @return true if gyroscope hardware test passes (result = 0x01), false if fails (result = 0x00)
+     */
+    bool selftest_gyro();
+    
+    /**
+     * @brief Perform microcontroller unit hardware self-test
+     * @return true if MCU hardware test passes (result = 0x01), false if fails (result = 0x00)
+     */
+    /**
+     * @brief Perform microcontroller unit hardware self-test
+     * @return true if MCU hardware test passes (result = 0x01), false if fails (result = 0x00)
+     */
+    bool selftest_mcu();
+    
+    // ========== SYSTEM STATUS AND ERROR CHECKING ==========
+    /**
+     * @brief Check if the system is running properly
+     * @return true if system is in operational state (status 5 or 6), false otherwise
+     * @details System status codes:
+     *          - 0: System idle, no operation being performed
+     *          - 1: System error
+     *          - 2: Initializing peripherals
+     *          - 3: System initialization
+     *          - 4: Executing self-test
+     *          - 5: Sensor fusion algorithm running (GOOD)
+     *          - 6: System running without fusion (GOOD)
+     */
+    bool check_system_status();
+    
+    /**
+     * @brief Check if there are any system errors
+     * @return true if no system errors detected (error code = 0), false if errors present
+     */
+    bool check_system_error();
+    
+    /**
+     * @brief Check if the main clock is running properly
+     * @return true if main clock is operational (status = 1), false otherwise
+     */
+    bool check_clock_status();
+    
+    /**
+     * @brief Get the specific system error code for diagnosis
+     * @return System error code (0 = no error, >0 = specific error condition)
+     */
+    uint8_t get_system_error_code();
+    
+    /**
+     * @brief Get the specific system status code for diagnosis
+     * @return System status code (see check_system_status() for code meanings)
+     */
+    /**
+     * @brief Get the specific system status code for diagnosis
+     * @return System status code (see check_system_status() for code meanings)
+     */
+    uint8_t get_system_status_code();
+    
+    // ========== OPERATION AND POWER MODE MANAGEMENT ==========
+    /**
+     * @brief Set the sensor operation mode
+     * @param mode Operation mode to set
+     * @return true if mode set successfully, false otherwise
+     * @details Available operation modes:
+     *          - BNO055_OPERATION_MODE_CONFIG (0x00): Configuration mode
+     *          - BNO055_OPERATION_MODE_ACCONLY (0x01): Accelerometer only
+     *          - BNO055_OPERATION_MODE_MAGONLY (0x02): Magnetometer only
+     *          - BNO055_OPERATION_MODE_GYRONLY (0x03): Gyroscope only
+     *          - BNO055_OPERATION_MODE_ACCMAG (0x04): Accelerometer + Magnetometer
+     *          - BNO055_OPERATION_MODE_ACCGYRO (0x05): Accelerometer + Gyroscope
+     *          - BNO055_OPERATION_MODE_MAGGYRO (0x06): Magnetometer + Gyroscope
+     *          - BNO055_OPERATION_MODE_AMG (0x07): All sensors, no fusion
+     *          - BNO055_OPERATION_MODE_IMUPLUS (0x08): IMU fusion (no magnetometer)
+     *          - BNO055_OPERATION_MODE_COMPASS (0x09): Compass mode
+     *          - BNO055_OPERATION_MODE_M4G (0x0A): M4G fusion mode
+     *          - BNO055_OPERATION_MODE_NDOF_FMC_OFF (0x0B): 9DOF without fast mag calibration
+     *          - BNO055_OPERATION_MODE_NDOF (0x0C): 9DOF fusion (recommended for rockets)
+     */
+    bool set_operation_mode(uint8_t mode);
+    
+    /**
+     * @brief Get the current sensor operation mode
+     * @param mode Pointer to store the current operation mode
+     * @return true if mode retrieved successfully, false otherwise
+     */
+    bool get_operation_mode(uint8_t* mode);
+    
+    /**
+     * @brief Set the sensor power mode
+     * @param mode Power mode to set
+     * @return true if power mode set successfully, false otherwise
+     * @details Available power modes:
+     *          - BNO055_POWER_MODE_NORMAL (0x00): Normal operation
+     *          - BNO055_POWER_MODE_LOWPOWER (0x01): Low power mode
+     *          - BNO055_POWER_MODE_SUSPEND (0x02): Suspend mode
+     */
+    bool set_power_mode(uint8_t mode);
+    
+    /**
+     * @brief Get the current sensor power mode
+     * @param mode Pointer to store the current power mode
+     * @return true if power mode retrieved successfully, false otherwise
+     */
+    bool get_power_mode(uint8_t* mode);
+    
+    /**
+     * @brief Set the accelerometer power mode
+     * @param mode Accelerometer power mode (0x00-0x05)
+     * @return true if power mode set successfully, false otherwise
+     */
+    bool set_accel_power_mode(uint8_t mode);
+    
+    /**
+     * @brief Set the magnetometer power mode
+     * @param mode Magnetometer power mode (0x00-0x03)
+     * @return true if power mode set successfully, false otherwise
+     */
+    bool set_mag_power_mode(uint8_t mode);
+    
+    /**
+     * @brief Set the gyroscope power mode
+     * @param mode Gyroscope power mode (0x00-0x04)
+     * @return true if power mode set successfully, false otherwise
+     */
+    /**
+     * @brief Set the gyroscope power mode
+     * @param mode Gyroscope power mode (0x00-0x04)
+     * @return true if power mode set successfully, false otherwise
+     */
+    bool set_gyro_power_mode(uint8_t mode);
+
+    // ========== SENSOR DATA READING FUNCTIONS ==========
+    
+    /**
+     * @brief Get the current accelerometer data
+     * @return Accelerometer data structure containing x, y, z acceleration values
+     */
+    struct bno055_accel_float_t get_accel();
+
+    /**
+     * @brief Get the current magnetometer data
+     * @return Magnetometer data structure containing x, y, z magnetic field values
+     */
+    struct bno055_mag_float_t get_mag();
+
+    /**
+     * @brief Get the current gyroscope data in degrees per second
+     * @return Gyroscope data structure containing x, y, z angular velocity values in degrees per second
+     */
+    struct bno055_gyro_float_t get_gyro_dps();
+
+    /**
+     * @brief Get the current gyroscope data in radians per second
+     * @return Gyroscope data structure containing x, y, z angular velocity values in radians per second
+     */
+    struct bno055_gyro_float_t get_gyro_rps();
+
+    /**
+     * @brief Get the current Euler angles in degrees
+     * @return Euler angles structure containing heading, roll, pitch in degrees
+     */
+    struct bno055_euler_float_t get_euler_deg();
+
+    /**
+     * @brief Get the current Euler angles in radians
+     * @return Euler angles structure containing heading, roll, pitch in radians
+     */
+    struct bno055_euler_float_t get_euler_rad();
+
+    /**
+     * @brief Get the current quaternion data
+     * @return Quaternion data structure containing x, y, z, w values
+     */
+    struct bno055_quaternion_t get_quaternion();
+
+    /**
+     * @brief Get the current linear acceleration data
+     * @return Linear acceleration data structure containing x, y, z acceleration values
+     */
+    struct bno055_linear_accel_t get_linear_accel();
+
+    /**
+     * @brief Get the current gravity vector data
+     * @return Gravity vector data structure containing x, y, z acceleration values
+     */
+    struct bno055_gravity_t get_gravity();
+
+    /**
+     * @brief Get the current temperature data
+     * @return Temperature in degrees Celsius
+     */
+    float get_temperature();
+};
+#endif
