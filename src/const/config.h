81efa84fae9365fc1af82168385c52d70d352a9f
Giacomo Antonelli <giacomo.antonelli3@gmail.com>
2025-07-24 11:42:58 +0200

    [BNO055Sensor] Add a new constant for the minimum sufficient calibration value

--- src/const/config.h
+++ src/const/config.h
@@ -1,3 +1,5 @@
+// Minimum calibration value to consider the sensor sufficiently calibrated, going from 0 to 3
+#define SUFFICIENT_SENSOR_CALIBRATION 2
 #define SENSOR_LOOKUP_MAX_ATTEMPTS 5
 #define SENSOR_LOOKUP_TIMEOUT 1000
 #define BME680_I2C_ADDR_1 0x77
